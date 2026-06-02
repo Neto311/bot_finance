2a52c81d09e7d3e264b5b04748239d585699098d
netocel <oncelotti@gmail.com>
2026-06-02 13:10:37 -0300

    feat: aumentando tempo de espera do start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 export PORT=${PORT:-10000}
 
 echo "Iniciando API na porta $PORT..."
-uvicorn main:app --host 0.0.0.0 --port $PORT &
+uvicorn main:app --host 0.0.0.0 --port $P0RT > api.log 2>&1 &
 
 echo "Aguardando API estabilizar..."
-sleep 5
+sleep 60
 
 echo "Iniciando Bot Telegram..."
 python3 services/bot_telegram.py
